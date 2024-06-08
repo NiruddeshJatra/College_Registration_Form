ccca0d9156ac1a1bd03789bde35df118d6fa00a5
NiruddeshJatra <nasifulalam1212gmail.com>
2024-06-09 01:06:14 +0600

    added database

--- main.py
+++ main.py
@@ -12,12 +12,14 @@ from PyQt6.QtWidgets import (
     QVBoxLayout, 
     QFormLayout,
     QButtonGroup,
+    QFileDialog,
     QCalendarWidget
 )
-from PyQt6.QtGui import QIcon
+from PyQt6.QtGui import QIcon, QPixmap
 from PyQt6.QtCore import Qt, QDate
 import sys
 import re
+import mysql.connector
 
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 650
@@ -82,6 +84,16 @@ class MyForm(QMainWindow):
         centralWidget.setLayout(self.generalLayout)
         
         self._setUpUI()
+        
+        
+    def loadDatabase(self):
+        self.mydb = mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            passwd = "password",
+            database = "student_database",
+        )
+        self.c = self.mydb.cursor()
 
     def _setUpUI(self):
         self.heading = QLabel("S T U D E N T   R E G I S T R A T I O N   F O R M")
@@ -93,15 +105,17 @@ class MyForm(QMainWindow):
             Qt.AlignmentFlag.AlignCenter
         )
         self.generalLayout.addWidget(self.heading)
-        
+
         self.formLayout = QFormLayout()
 
         self.nameLabel = QLabel("Name")
         self.nameLabel.setFixedWidth(LABEL_WIDTH)
         self.nameEditLayout = QHBoxLayout()
         self.firstNameEdit = QLineEdit()
+        self.firstName = self.firstNameEdit.text()
         self.firstNameEdit.setPlaceholderText("First Name")
         self.lastNameEdit = QLineEdit()
+        self.lastName = self.lastNameEdit.text()
         self.lastNameEdit.setPlaceholderText("Last Name")
         self.nameEditLayout.addWidget(self.firstNameEdit)
         self.nameEditLayout.addWidget(self.lastNameEdit)
@@ -115,10 +129,12 @@ class MyForm(QMainWindow):
         self.genderGroup = QButtonGroup()
         self.genderGroup.addButton(self.maleSelect)
         self.genderGroup.addButton(self.femaleSelect)
+        self.maleSelect.toggled.connect(self.onToggled)
+        self.femaleSelect.toggled.connect(self.onToggled)
         self.genderButtonLayout.addWidget(self.maleSelect)
         self.genderButtonLayout.addWidget(self.femaleSelect)
         self.formLayout.addRow(self.genderLabel, self.genderButtonLayout)
-        
+
         self.dateLabel = QLabel("Date of Birth")
         self.dateLabel.setFixedWidth(LABEL_WIDTH)
         self.dateEdit = QLineEdit()
@@ -136,22 +152,25 @@ class MyForm(QMainWindow):
         self.formLayout.addRow(self.dateLabel, self.dateEdit)
         self.isCalendarOpen = False
         self.dateEdit.mousePressEvent = self.mousePressEvent
-        
+
         self.ageLabel = QLabel("Age")
         self.ageLabel.setFixedWidth(LABEL_WIDTH)
         self.ageEdit = QLineEdit()
+        self.age = self.ageEdit.text()
         self.formLayout.addRow(self.ageLabel, self.ageEdit)
-        
+
         self.emailLabel = QLabel("Email Address")
         self.emailLabel.setFixedWidth(LABEL_WIDTH)
         self.emailEdit = QLineEdit()
+        self.email = self.emailEdit.text()
         self.formLayout.addRow(self.emailLabel, self.emailEdit)
-        
+
         self.passwordLabel = QLabel("Password")
         self.passwordLabel.setFixedWidth(LABEL_WIDTH)
         self.passwordEdit = QLineEdit()
+        self.password = self.passwordEdit.text()
         self.formLayout.addRow(self.passwordLabel, self.passwordEdit)
-        
+
         self.phoneLabel = QLabel("Phone Number")
         self.phoneLabel.setFixedWidth(LABEL_WIDTH)
         self.phoneLayout = QHBoxLayout()
@@ -160,10 +179,11 @@ class MyForm(QMainWindow):
         self.numberOptions.addItems(codes)
         self.numberOptions.setFixedWidth(100)
         self.phoneEdit = QLineEdit()
+        self.phoneNo = self.numberOptions.currentText() + self.phoneEdit.text()
         self.phoneLayout.addWidget(self.numberOptions)
         self.phoneLayout.addWidget(self.phoneEdit)
         self.formLayout.addRow(self.phoneLabel, self.phoneLayout)
-        
+
         self.addressLabel = QLabel("Address")
         self.addressLabel.setFixedWidth(LABEL_WIDTH)
         self.addressEditLayout = QHBoxLayout()
@@ -174,7 +194,7 @@ class MyForm(QMainWindow):
         self.addressEditLayout.addWidget(self.villageEdit)
         self.addressEditLayout.addWidget(self.thanaEdit)
         self.formLayout.addRow(self.addressLabel, self.addressEditLayout)
-        
+
         self.address2Label = QLabel()
         self.address2Label.setFixedWidth(LABEL_WIDTH)
         self.address2EditLayout = QHBoxLayout()
@@ -182,19 +202,21 @@ class MyForm(QMainWindow):
         self.districtEdit = QLineEdit()
         self.postOfficeEdit.setPlaceholderText("Post Office")
         self.districtEdit.setPlaceholderText("District")
+        self.address = f"{self.villageEdit.text()}, {self.thanaEdit.text()}, {self.postOfficeEdit.text()}, {self.districtEdit.text()}"
         self.address2EditLayout.addWidget(self.postOfficeEdit)
         self.address2EditLayout.addWidget(self.districtEdit)
         self.formLayout.addRow(self.address2Label, self.address2EditLayout)
-        
+
         self.hobbyLabel = QLabel("Hobbies")
         self.hobbyLabel.setFixedWidth(LABEL_WIDTH)
         self.hobbyEdit = QComboBox()
         self.hobbyEdit.setFixedSize(200, 30)
         options = ["Drawing", "Decorating", "Playing", "Singing", "Others"]
         self.hobbyEdit.addItems(options)
+        self.hobbies = self.hobbyEdit.currentText()
         # self.hobbyEdit.activated.connect(self.showDropdown)
         self.formLayout.addRow(self.hobbyLabel, self.hobbyEdit)
-        
+
         self.photoLayout = QHBoxLayout()
         self.photoLabel = QLabel("Upload Photo")
         self.photoLabel.setFixedWidth(LABEL_WIDTH)
@@ -202,15 +224,17 @@ class MyForm(QMainWindow):
         self.photoSizeLabel.setStyleSheet("font-size: 8pt; color: #FE231F; margin: 1px;")
         self.photoButton = QPushButton("Choose File")
         self.photoButton.setStyleSheet("margin-left: 5px; margin-right: 5px;")
+        self.photoButton.clicked.connect(self.uploadImage)
         self.photoUploadLabel = QLabel("No File Chosen")
         self.photoUploadLabel.setStyleSheet("margin-left: 10px; margin-right: 50px;")
         self.photoLayout.addWidget(self.photoSizeLabel)
         self.photoLayout.addWidget(self.photoButton)
         self.photoLayout.addWidget(self.photoUploadLabel)
         self.formLayout.addRow(self.photoLabel, self.photoLayout)
-        
+
         self.checkBox = QCheckBox("   I hereby declare that the above information provided is true and correct.")
-        
+        self.checkBox.stateChanged.connect(self.onStageChanged)
+
         self.buttonLayout = QHBoxLayout()
         self.submit = QPushButton("Submit")
         self.reset = QPushButton("Reset")
@@ -218,9 +242,11 @@ class MyForm(QMainWindow):
         self.reset.setStyleSheet("margin-top: 15px; margin-bottom: 15px;")
         self.submit.setFixedWidth(BUTTON_WIDTH)
         self.reset.setFixedWidth(BUTTON_WIDTH)
+        self.submit.setEnabled(False)
+        self.submit.clicked.connect(self.addProfile)
         self.buttonLayout.addWidget(self.submit)
         self.buttonLayout.addWidget(self.reset)
-        
+
         self.generalLayout.addLayout(self.formLayout)
         self.generalLayout.addWidget(self.checkBox)
         self.generalLayout.addLayout(self.buttonLayout)
@@ -254,8 +280,41 @@ class MyForm(QMainWindow):
                 self.calendar.hide()
             
     def onDateSelected(self):
-        selectedDate = self.calendar.selectedDate().toString("dd-MM-yyyy")
-        self.dateEdit.setText(selectedDate)
+        self.selectedDate = self.calendar.selectedDate().toString("yyyy-MM-dd")
+        self.dateEdit.setText(self.selectedDate)
+        
+    def onStageChanged(self, state):
+        if state == 2:
+            self.submit.setEnabled(True)
+        else:
+            self.submit.setEnabled(False)
+            
+    def onToggled(self):
+        if self.maleSelect.isChecked():
+            self.gender = "male"
+        elif self.femaleSelect.isChecked():
+            self.gender = "female"
+            
+    def uploadImage(self):
+        imagePath, _ = QFileDialog.getOpenFileName(self, 'Open Image File', '', 'Image Files (*.png *.jpg *.bmp)')
+        
+        if imagePath:
+            pixmap = QPixmap(imagePath)
+            self.photoUploadLabel.setPixmap(pixmap)
+
+            with open(imagePath, 'rb') as file:
+                self.imageData = file.read()
+
+            
+    def addProfile(self):
+        if self.submit.isEnabled:
+            self.loadDatabase()
+            self.c.execute("""
+                    INSERT INTO students (first_name, last_name, gender, date_of_birth, age, email, password, phone_no, address, hobbies, photo)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """, (self.firstName, self.lastName, self.gender, self.selectedDate, self.age, self.email, self.password, self.phoneNo, self.address, self.hobbies, self.imageData,))
+            self.mydb.commit()
+            self.mydb.close()
         
 def main():
     app = QApplication(sys.argv)
